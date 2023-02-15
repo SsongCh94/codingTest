587c5798f59bcc80b02a0f8ddd82f5cb35ba4950
SsongCh94 <thddkwl94@gmail.com>
2023-02-15 15:13:16 +0900

    위 두개 코드중 다른점과 그걸 이해해주자

--- Test2.js
+++ Test2.js
@@ -7,10 +7,20 @@ function solution(num) {
 
 }
 
+function solution(num) {
+    if (num%2){
+        return "Odd"
+    } else {
+        return "Even"
+    }
+
+}
+
+
 // 아래의 한줄코드로도 가능하다!
 
 function solution(num) {
     return num%2 ? "Odd" : "Even";
 }
 
-// 위 코드에서 num%2 가 0 이면 false 값이니까 Even을 반환하고, 1이면 true 가 되어 Odd 를 반환한다. 
+// 위 코드에서 num%2 가 0 이면 false 값이니까 Even을 반환하고, 1이면 true 가 되어 Odd 를 반환한다.
