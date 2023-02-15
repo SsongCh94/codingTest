463272a9871bfd1ed34e08d02c1856483fc23be7
SsongCh94 <thddkwl94@gmail.com>
2023-02-15 14:20:37 +0900

    다른 풀이 추가 (다른사람 답)

--- codingTest2.js
+++ codingTest2.js
@@ -6,3 +6,11 @@ function solution(num) {
     }
 
 }
+
+// 아래의 한줄코드로도 가능하다!
+
+function solution(num) {
+    return num%2 ? "Odd" : "Even";
+}
+
+// 위 코드에서 num%2 가 0 이면 false 값이니까 Even을 반환하고, 1이면 true 가 되어 Odd 를 반환한다.
