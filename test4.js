0d898566315654b3cac0843cfbfdcab7600d3e51
SsongCh94 <thddkwl94@gmail.com>
2023-02-15 15:59:54 +0900

    test 4 간단한 방법.... 기본부터 철저히....

--- test4.js
+++ test4.js
@@ -15,3 +15,12 @@ function solution(a, b) {
 
   return result;
 }
+
+// 훨씬 간단한 방법.... for 반복문의 특성을 이용하자
+function solution(a, b) {
+  let x = 0;
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) x += i;
+  return x;
+}
+
+//이렇게 간단한걸.....
