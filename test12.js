798b165857288c634a89a0fa604e62172906b653
SsongCh94 <thddkwl94@gmail.com>
2023-02-17 01:28:59 +0900

    부족한 금액 계산하기 / 깔끔하게 압축

--- test12.js
+++ test12.js
@@ -2,11 +2,10 @@
 
 function solution(price, money, count) {
     var answer = 0;
-var result = 0;
-for (let i = 1; i <= count; i++) {
-    answer += price*i;
-}
-(money - answer >= 0) ? result = 0 : result = Math.abs(money - answer);
-
-return result;
-}
+    for (let i = 1; i <= count; i++) {
+      answer += price * i;
+    }
+  
+    return answer - money > 0 ? answer - money : 0;
+  }
+  
