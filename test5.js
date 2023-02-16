bcedea2cf778d9b79e9011d3dc216fed51545672
SsongCh94 <thddkwl94@gmail.com>
2023-02-16 09:05:07 +0900

    문자열을 정수로 바꾸기 연산자를 통한 풀이

--- test5.js
+++ test5.js
@@ -1,8 +1,13 @@
 // 문자열을 정수로 바꾸기
 
 function solution(s) {
-  let answer = Number(s);
-  return answer;
+  return Number(s);
 }
 
 // Number 메소드로 숫자화해준다.
+
+function solution(s) {
+  return +s;  // s-0 s/1  s*1 등등
+}
+
+// 연산자를 이용한 풀이.
