d953f178ee36bff9d9d2a7164fdb05d6930cd131
SsongCh94 <thddkwl94@gmail.com>
2023-02-17 01:12:12 +0900

    x만큼 간격이 있는 n개의 숫자 / 코드 압축

--- test11.js
+++ test11.js
@@ -1,10 +1,9 @@
 //x만큼 간격이 있는 n개의 숫자
 
 function solution(x, n) {
-    var row = [x, n];
     var answer = [];
-    for (let i = 0; i < n; i++) {
-        answer.push(x+x*i);
+    for (let i = 1; i <= n; i++) {
+        answer.push(x*i);
     }
     return answer;
 }
