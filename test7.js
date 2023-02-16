1a514d0f7f5913d2886e27b99ac27857937e14f0
SsongCh94 <thddkwl94@gmail.com>
2023-02-16 10:28:53 +0900

    //음양 더하기

---
+++ test7.js
@@ -0,0 +1,19 @@
+//음양 더하기
+
+function solution(absolutes, signs) {
+  var answer = 0;
+  for (let i = 0; i < absolutes.length; i++) {
+    signs[i] ? (answer += absolutes[i]) : (answer -= absolutes[i]);
+  }
+  return answer;
+}
+
+// for 문 map 으로 변환
+
+function solution(absolutes, signs) {
+  var answer = 0;
+  signs.map((x, index) => {
+    x ? (answer += absolutes[index]) : (answer -= absolutes[index]);
+  });
+  return answer;
+}
