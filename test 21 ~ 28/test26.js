dbc5e09cebfbfaa23f0591d4c61846d0bce1db35
SsongCh94 <thddkwl94@gmail.com>
2023-02-17 17:26:35 +0900

    제일 작은 수 제거하기

---
+++ test 21 ~ 28/test26.js
@@ -0,0 +1,34 @@
+// 제일 작은 수 제거하기
+
+
+function solution(arr) {
+  var answer = [];
+  var min = arr[0];
+
+  if (arr.length > 1) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] <= min) {
+        min = arr[i];
+      }
+    }
+
+    arr.splice(arr.indexOf(min), 1);
+
+    answer = arr;
+  } else {
+    answer.push(-1);
+  }
+
+  return answer;
+}
+
+
+// 답 참고... 깔끔해짐, Math.min 메서드 추가
+
+function solution(arr) {
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+
+  if (arr.length < 1) arr.push(-1);
+  console.log(arr);
+  return arr;
+}
