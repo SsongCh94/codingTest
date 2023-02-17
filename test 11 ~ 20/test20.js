f34c981da1b8e9ddb12ef327537150b229c5c236
SsongCh94 <thddkwl94@gmail.com>
2023-02-18 00:37:06 +0900

    완주하지 못한 선수 // for in 메서드 추가, 필요없던부분 삭제

--- test 11 ~ 20/test20.js
+++ test 11 ~ 20/test20.js
@@ -1,6 +1,5 @@
 //완주하지 못한 선수
 
-
 function solution(participant, completion) {
   var sortPar = participant.sort();
   var sortCom = completion.sort();
@@ -12,5 +11,15 @@ function solution(participant, completion) {
   }
 }
 
+// sort() 생각도 못함,
 
-// sort() 생각도 못함, 
+// 요약버전, for in 메서드 추가, 필요없던부분 삭제
+
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+
+  for (i in participant) {
+    if (completion[i] !== participant[i]) return participant[i];
+  }
+}
