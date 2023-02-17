0f9497f22ef716e4c266766dd92c11212f552eb4
SsongCh94 <thddkwl94@gmail.com>
2023-02-17 09:44:35 +0900

    내적

---
+++ test15.js
@@ -0,0 +1,20 @@
+// 내적
+
+function solution(a, b) {
+  var answer = a.reduce((acc, cur, idx) => {
+    acc += cur * b[idx];
+    return acc;
+  }, 0);
+  return answer;
+}
+
+// return acc 까먹지 말기..
+
+function solution(a, b) {
+  var answer = 0;
+  for (let i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
+  }
+  console.log(answer);
+  return answer;
+}
