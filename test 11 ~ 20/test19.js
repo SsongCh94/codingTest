6e05c7506f7c2572a1072fa59bb604d6eab70326
SsongCh94 <thddkwl94@gmail.com>
2023-02-17 23:37:35 +0900

    수박수박수박수박수 // 1줄코드 추가

--- test 11 ~ 20/test19.js
+++ test 11 ~ 20/test19.js
@@ -1,5 +1,11 @@
 // 수박수박수박수박수
 
+// 가독성은 개나준 1줄코드
+
+solution = n => {return n % 2 ? '수박'.repeat((n-1)/2) + '수' : '수박'.repeat(n/2);}
+
+// 첫트라이
+
 function solution(n) {
   if (n % 2 == 0) {
     return "수박".repeat(n / 2);
